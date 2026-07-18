49a37f83e9e4215422141ca7f249de342214e192
Noan Jard <noan.pujol@gmail.com>
2026-07-18 15:52:29 +0200

    data_prcoessing program started

---
+++ process_dataset.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+columns = [
+    "id",
+    "diagnosis",
+    "radius_mean",
+    "texture_mean",
+    "perimeter_mean",
+    "area_mean",
+    "smoothness_mean",
+    "compactness_mean",
+    "concavity_mean",
+    "concave_points_mean",
+    "symmetry_mean",
+    "fractal_dimension_mean",
+    "radius_se",
+    "texture_se",
+    "perimeter_se",
+    "area_se",
+    "smoothness_se",
+    "compactness_se",
+    "concavity_se",
+    "concave_points_se",
+    "symmetry_se",
+    "fractal_dimension_se",
+    "radius_worst",
+    "texture_worst",
+    "perimeter_worst",
+    "area_worst",
+    "smoothness_worst",
+    "compactness_worst",
+    "concavity_worst",
+    "concave_points_worst",
+    "symmetry_worst",
+    "fractal_dimension_worst",
+]
+
+def create_prediction_dataset(dataset):
+    # print(dataset)
+    prediction_dataset = dataset.drop(['diagnosis'], axis=1)
+    prediction_dataset.to_csv("prediction_dataset.csv")
+    return
+
+def create_training_dataset(dataset):
+    dataset.to_csv("training_dataset.csv")
+    return
+
+def main():
+    dataset = pd.read_csv("data.csv")
+    dataset.columns = columns
+    # print(dataset)
+    create_prediction_dataset(dataset)
+    create_training_dataset(dataset)
+    return
+
+
+if __name__ == "__main__":
+    main()
